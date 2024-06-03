4c85fc11475dd14e7de0701c2fa7b7e2614ee934
udaykiran <40133834+uday7981@users.noreply.github.com>
2024-06-03 05:56:32 +0530

    init

---
+++ server/routes/admin.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/me", (req, res) => {
+
+
+});
+
+
+router.post("/signup", (req, res) => {
+
+});
+
+
+router.post("/signin", (req, res) => {
+
+});
+
+
+router.post("/courses", (req, res) => {
+
+});
+
+router.put("/courses/:courseId", (req, res) => {
+
+});
+
+
+router.get("/courses", (req, res) => {
+
+});
+
+
+router.get("/courses/:courseId", (req, res) => {
+
+});
+
+module.exports = router
